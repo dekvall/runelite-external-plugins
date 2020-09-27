773c859d639046d2da4b9f01203c68fb4593ae34
dekvall <dkvldev@gmail.com>
2020-09-28 00:50:58 +0200

    remove custom menuentry removals

--- src/main/java/dekvall/notempty/NotEmptyPlugin.java
+++ src/main/java/dekvall/notempty/NotEmptyPlugin.java
@@ -21,49 +21,25 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Not Empty"
+	name = "Not Empty",
+	description = "Never empty potions again!"
 )
 public class NotEmptyPlugin extends Plugin
 {
-	static final String CONFIG_GROUP = "notempty";
 
 	@Inject
 	private Client client;
 
-	@Inject
-	private NotEmptyConfig config;
-
-	private Set<String> removedEntries;
-
 	@Override
 	protected void startUp() throws Exception
 	{
-		createFilter();
 		log.info("Not Empty started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		removedEntries.clear();
 		log.info("Not Empty stopped!");
-	}
-
-	@Subscribe
-	public void onConfigChanged(ConfigChanged event)
-	{
-		if (event.getGroup().equals(CONFIG_GROUP))
-		{
-			createFilter();
-		}
-	}
-
-	private void createFilter()
-	{
-		removedEntries = Text.fromCSV(config.removedEntries())
-				.stream()
-				.map(String::toLowerCase)
-				.collect(Collectors.toSet());
 	}
 
 	@Subscribe
@@ -77,36 +53,12 @@ public class NotEmptyPlugin extends Plugin
 			int type = entry.getType();
 			String option = entry.getOption().toLowerCase();
 
-			if (!isItemEntry(type) || !removedEntries.contains(option))
+			if (type != MenuAction.ITEM_FOURTH_OPTION.getId()
+				|| !"empty".equals(option))
 			{
 				cleaned.add(entry);
 			}
 		}
 		client.setMenuEntries(cleaned.toArray(new MenuEntry[0]));
-	}
-
-	private boolean isItemEntry(int type)
-	{
-		MenuAction action = MenuAction.of(type);
-
-		switch (action)
-		{
-			// Not using USE or FIRST_OPTION to prevent potential abuse
-			case ITEM_SECOND_OPTION:
-			case ITEM_THIRD_OPTION:
-			case ITEM_FOURTH_OPTION:
-			case ITEM_FIFTH_OPTION:
-			case ITEM_DROP:
-			case EXAMINE_ITEM:
-				return true;
-			default:
-				return false;
-		}
-	}
-
-	@Provides
-	NotEmptyConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(NotEmptyConfig.class);
 	}
 }
