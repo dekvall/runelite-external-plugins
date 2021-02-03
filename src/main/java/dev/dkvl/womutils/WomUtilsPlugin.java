338b9a27e88707ca4b25635653c078345175fe81
dekvall <dkvldev@gmail.com>
2021-02-03 21:00:05 +0100

    list fix

--- src/main/java/dev/dkvl/womutils/WomUtilsPlugin.java
+++ src/main/java/dev/dkvl/womutils/WomUtilsPlugin.java
@@ -17,6 +17,7 @@ import java.lang.reflect.Type;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.LinkedBlockingQueue;
