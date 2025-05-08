8c9a8c8a4f060143b0b2dbaaaaa5d797447e0f7c
notheowner <86596679+notheowner@users.noreply.github.com>
2025-05-08 14:08:04 +0300

    Gotify URL example and default priority(I know how to use git I swear

--- src/main/java/dekvall/pushnotification/PushNotificationsConfig.java
+++ src/main/java/dekvall/pushnotification/PushNotificationsConfig.java
@@ -56,7 +56,7 @@ public interface PushNotificationsConfig extends Config
 	@ConfigItem(
 		keyName = "gotify_url",
 		name = "Gotify URL",
-		description = "URL for Gotify server",
+		description = "URL for Gotify server, example: http://10.0.0.30:8080/message",
 		section = gotifySection
 	)
 	String gotify_url();
@@ -75,5 +75,7 @@ public interface PushNotificationsConfig extends Config
 		description = "Priority for Gotify notification",
 		section = gotifySection
 	)
-	int gotify_priority();
+	default int gotify_priority(
+		return 5;
+	);
 }
