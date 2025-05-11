734885de3d534ceba60c50c18779610496ac3dcb
notheowner <86596679+notheowner@users.noreply.github.com>
2025-05-12 01:22:00 +0300

    Revert "Append /message and update example URL"
    This reverts commit 0fb05f1cceb6daa827e67af33f0070a7b6e007c5.

--- src/main/java/dekvall/pushnotification/PushNotificationsPlugin.java
+++ src/main/java/dekvall/pushnotification/PushNotificationsPlugin.java
@@ -132,12 +132,11 @@ public class PushNotificationsPlugin extends Plugin
 
 		if (parsedUrl == null)
 		{
-			log.warn("Invalid Gotify URL, expected format: http or https://<host>:<port>");
+			log.warn("Invalid Gotify URL, expected format: http or https://<host>:<port>/message");
 			return;
 		}
 
 		HttpUrl url = parsedUrl.newBuilder()
-			.addPathSegment("message")
 			.addQueryParameter("token", config.gotify_token())
 			.build();
 		
