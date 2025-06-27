3c5084d757b8a880bca50e347aa34c75ad880ab2
dekvall <dkvldev@gmail.com>
2025-06-28 00:57:43 +0200

    resize world switcher logout button

--- src/main/java/dev/dkvl/largelogout/LargeLogoutPlugin.java
+++ src/main/java/dev/dkvl/largelogout/LargeLogoutPlugin.java
@@ -33,7 +33,14 @@ public class LargeLogoutPlugin extends Plugin
 	private static final int ORIG_WORLD_SWITCHER_BUTTON_HEIGHT = 36;
 	private static final int WIDGET_SPACING = 10;
 
+	private static final int ORIG_WORLD_SWITCHER_MIDDLE_HEIGHT = 55;
+	private static final int ORIG_WORLD_SWITCHER_BOTTOM_HEIGHT = 32;
+	private static final int ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_WIDTH = 21;
+	private static final int ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_HEIGHT = 30;
+	private static final int ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_X = 2;
+
 	private static final int SCRIPT_LOGOUT_LAYOUT_UPDATE = 2243;
+	private static final int SCRIPT_WORLD_SWITCHER_DRAW = 892;
 
 	@Inject
 	private Client client;
@@ -50,6 +57,11 @@ public class LargeLogoutPlugin extends Plugin
 		if (client.getGameState() == GameState.LOGGED_IN)
 		{
 			clientThread.invokeLater(this::enlargeLogoutButton);
+			clientThread.invokeLater(() ->
+			{
+				enlargeWorldSwitcherLogoutButton();
+				updateUniverse(InterfaceID.Worldswitcher.UNIVERSE);
+			});
 		}
 	}
 
@@ -59,6 +71,11 @@ public class LargeLogoutPlugin extends Plugin
 		if (client.getGameState() == GameState.LOGGED_IN)
 		{
 			clientThread.invokeLater(this::restoreLogoutLayout);
+			clientThread.invokeLater(() ->
+			{
+				restoreWorldSwitcherLayout();
+				updateUniverse(InterfaceID.Worldswitcher.UNIVERSE);
+			});
 		}
 	}
 
@@ -69,46 +86,15 @@ public class LargeLogoutPlugin extends Plugin
 		{
 			enlargeLogoutButton();
 		}
-	}
-
-	private void restoreLogoutLayout()
-	{
-		if (!canResize())
+		else if (event.getScriptId() == SCRIPT_WORLD_SWITCHER_DRAW)
 		{
-			return;
+			enlargeWorldSwitcherLogoutButton();
 		}
-
-		client.getWidget(InterfaceID.Logout.SATISFACTION).setHidden(false);
-		client.getWidget(InterfaceID.Logout.LOGOUT_TEXT).setHidden(false);
-
-		client.getWidget(InterfaceID.Logout.LOGOUT_BUTTONS)
-			.setHeightMode(WidgetSizeMode.ABSOLUTE)
-			.setOriginalHeight(ORIG_BUTTONS_PANE_HEIGHT)
-			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
-			.setOriginalY(ORIG_BUTTONS_PANE_Y_OFFSET)
-			.revalidate();
-
-		client.getWidget(InterfaceID.Logout.WORLD_SWITCHER).
-			setYPositionMode(WidgetPositionMode.ABSOLUTE_CENTER)
-			.setOriginalY(0)
-			.revalidate();
-
-		Widget logoutButton = client.getWidget(InterfaceID.Logout.LOGOUT);
-
-		logoutButton
-			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
-			.setHeightMode(WidgetSizeMode.ABSOLUTE)
-			.setOriginalHeight(ORIG_LOGOUT_BUTTON_HEIGHT)
-			.setWidthMode(WidgetSizeMode.ABSOLUTE)
-			.setOriginalWidth(ORIG_LOGOUT_BUTTON_WIDTH)
-			.revalidate();
-
-		scaleButton(logoutButton, ORIG_CORNER_SIDE);
 	}
 
 	private void enlargeLogoutButton()
 	{
-		if (!canResize())
+		if (!canResizeLogout())
 		{
 			return;
 		}
@@ -135,16 +121,177 @@ public class LargeLogoutPlugin extends Plugin
 		scaleButton(logoutButton, logoutButton.getWidth() * 5 / 6);
 	}
 
-	private boolean canResize()
+	private void restoreLogoutLayout()
 	{
-		boolean layoutIsExpected = layoutIsExpected();
-		boolean isBetaWorld = client.getWorldType().contains(WorldType.BETA_WORLD)
-			|| client.getWorldType().contains(WorldType.NOSAVE_MODE);
+		if (!canResizeLogout())
+		{
+			return;
+		}
+
+		client.getWidget(InterfaceID.Logout.SATISFACTION).setHidden(false);
+		client.getWidget(InterfaceID.Logout.LOGOUT_TEXT).setHidden(false);
+
+		client.getWidget(InterfaceID.Logout.LOGOUT_BUTTONS)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE)
+			.setOriginalHeight(ORIG_BUTTONS_PANE_HEIGHT)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
+			.setOriginalY(ORIG_BUTTONS_PANE_Y_OFFSET)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Logout.WORLD_SWITCHER)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_CENTER)
+			.setOriginalY(0)
+			.revalidate();
+
+		Widget logoutButton = client.getWidget(InterfaceID.Logout.LOGOUT);
+
+		logoutButton
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE)
+			.setOriginalHeight(ORIG_LOGOUT_BUTTON_HEIGHT)
+			.setWidthMode(WidgetSizeMode.ABSOLUTE)
+			.setOriginalWidth(ORIG_LOGOUT_BUTTON_WIDTH)
+			.revalidate();
+
+		scaleButton(logoutButton, ORIG_CORNER_SIDE);
+	}
+
+	private void enlargeWorldSwitcherLogoutButton()
+	{
+		if (!canResizeWorldSwitcher())
+		{
+			return;
+		}
+
+		client.getWidget(InterfaceID.Worldswitcher.MIDDLE)
+			.setOriginalHeight(116)
+			.revalidateScroll();
+
+		client.getWidget(InterfaceID.Worldswitcher.BOTTOM)
+			.setOriginalHeight(93)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.LOGOUT)
+			.setXPositionMode(WidgetPositionMode.ABSOLUTE_CENTER)
+			.setOriginalX(0)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
+			.setOriginalWidth(0)
+			.setOriginalHeight(32)
+			.setWidthMode(WidgetSizeMode.MINUS)
+			.setHeightMode(WidgetSizeMode.MINUS)
+			.setSpriteTiling(true)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.FAVOURITE_1)
+			.setOriginalWidth(0)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE)
+			.setOriginalHeight(16)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.FAVOURITE_2)
+			.setOriginalWidth(0)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE)
+			.setOriginalHeight(16)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP)
+			.setOriginalY(15)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.NOFAVOURITES)
+			.setOriginalWidth(0)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP)
+			.setOriginalHeight(32)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE)
+			.revalidate();
+	}
+
+	private void restoreWorldSwitcherLayout()
+	{
+		if (!canResizeWorldSwitcher())
+		{
+			return;
+		}
+
+		client.getWidget(InterfaceID.Worldswitcher.MIDDLE)
+			.setOriginalHeight(ORIG_WORLD_SWITCHER_MIDDLE_HEIGHT)
+			.revalidateScroll();
+
+		client.getWidget(InterfaceID.Worldswitcher.BOTTOM)
+			.setOriginalHeight(ORIG_WORLD_SWITCHER_BOTTOM_HEIGHT)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.LOGOUT)
+			.setXPositionMode(WidgetPositionMode.ABSOLUTE_RIGHT)
+			.setOriginalX(ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_X)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_CENTER)
+			.setOriginalWidth(ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_WIDTH)
+			.setOriginalHeight(ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_HEIGHT)
+			.setWidthMode(WidgetSizeMode.ABSOLUTE)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE)
+			.setSpriteTiling(false)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.FAVOURITE_1)
+			.setOriginalWidth(ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_WIDTH)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE_16384THS)
+			.setOriginalHeight(8192)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.FAVOURITE_2)
+			.setOriginalWidth(ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_WIDTH)
+			.setHeightMode(WidgetSizeMode.ABSOLUTE_16384THS)
+			.setOriginalHeight(8192)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
+			.setOriginalY(0)
+			.revalidate();
+
+		client.getWidget(InterfaceID.Worldswitcher.NOFAVOURITES)
+			.setOriginalWidth(ORIG_WORLD_SWITCHER_LOGOUT_BUTTON_WIDTH)
+			.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM)
+			.setOriginalHeight(0)
+			.setHeightMode(WidgetSizeMode.MINUS)
+			.revalidate();
+	}
+
+	private boolean canResizeWorldSwitcher()
+	{
+		boolean layoutIsExpected = worldSwitcherLayoutIsExpected();
+		boolean isBetaWorld = isBetaWorld();
 
 		return layoutIsExpected && !isBetaWorld;
 	}
 
-	private boolean layoutIsExpected()
+	private boolean worldSwitcherLayoutIsExpected()
+	{
+		Widget layout = client.getWidget(InterfaceID.Worldswitcher.UNIVERSE);
+		if (layout == null)
+		{
+			return false;
+		}
+
+		Widget[] children = layout.getStaticChildren();
+		if (children == null)
+		{
+			return false;
+		}
+
+		return children.length == 5
+			&& Arrays.stream(children)
+			.allMatch(w -> w.getId() == InterfaceID.Worldswitcher.UNIVERSE_GRAPHIC0
+				|| w.getId() == InterfaceID.Worldswitcher.TOP
+				|| w.getId() == InterfaceID.Worldswitcher.MIDDLE
+				|| w.getId() == InterfaceID.Worldswitcher.BOTTOM
+				|| w.getId() == InterfaceID.Worldswitcher.TOOLTIP);
+	}
+
+	private boolean canResizeLogout()
+	{
+		boolean layoutIsExpected = logoutLayoutIsExpected();
+		boolean isBetaWorld = isBetaWorld();
+
+		return layoutIsExpected && !isBetaWorld;
+	}
+
+	private boolean logoutLayoutIsExpected()
 	{
 		Widget layout = client.getWidget(InterfaceID.Logout.UNIVERSE);
 		if (layout == null)
@@ -202,6 +349,28 @@ public class LargeLogoutPlugin extends Plugin
 			.setOriginalWidth(width)
 			.setSpriteTiling(false)
 			.revalidate();
+	}
+
+	private void updateUniverse(int widgetId)
+	{
+		Widget w = client.getWidget(widgetId);
+		if (w == null)
+		{
+			return;
+		}
+
+		Object[] args = w.getOnVarTransmitListener();
+		if (args == null)
+		{
+			return;
+		}
+		client.runScript(args);
+	}
+
+	private boolean isBetaWorld()
+	{
+		return client.getWorldType().contains(WorldType.BETA_WORLD)
+			|| client.getWorldType().contains(WorldType.NOSAVE_MODE);
 	}
 
 	@Provides
